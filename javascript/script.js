1771bf935ed95969fa96a460d9be0e1cba8bb780
PayalPatel03 <p6579113@gmail.com>
2025-04-11 17:53:33 +0530

    done

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,27 @@
+const apiKey = 'bf938dffdd0719fd29c28fa941a90c6a';
+const cities = ['London', 'New York', 'Tokyo', 'Delhi', 'Turkey','Rome','Barcelona','Bali','Singapore','Navsari','Port Louis']; 
 
+let citiesDropdown = document.getElementById('cities');
+let description = document.getElementById('description');
+let temp = document.getElementById('temp');
 
-let weather=document.getElementById('weather');
+cities.forEach((city) => {
+    let option = document.createElement('option');
+    option.value = city;
+    option.innerText = city;
+    citiesDropdown.append(option);
+});
+
+const displayWeather = () => {
+    let selectedCity = citiesDropdown.value;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${apiKey}&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+        description.innerText = `Weather: ${data.weather[0].description}`;
+        temp.innerText = `Temperature: ${data.main.temp}°C`;
+    })
+    .catch(error => {
+        console.log('Error fetching weather:', error);
+    });
+}
